dd8397424bf6cc297b904713fb69c83cfba45265
apsamade <bouderga.abdessamade@gmail.com>
2023-04-26 17:51:11 +0200

    maj clear

--- server.js
+++ server.js
@@ -9,19 +9,7 @@ const PORT = process.env.PORT
 
 // connect to mongoDB
 const dbURI = process.env.dbURI
-// mongodb+srv:// est le protocole utilisé pour se connecter à MongoDB en utilisant le service DNS (Domain Name System).
 
-// cours-test est le nom d'utilisateur utilisé pour se connecter à la base de données.
-
-// 7dtfj2xgKJXFLd1E est le mot de passe utilisé pour se connecter à la base de données.
-
-// bouderga.el84tpe.mongodb.net est l'URL de la base de données à laquelle vous voulez vous connecter.
-
-// test_mongo est le nom de la base de données à laquelle vous voulez vous connecter.
-
-// retryWrites=true indique que les opérations d'écriture seront automatiquement réessayées en cas d'échec.
-
-// w=majority indique que les opérations d'écriture doivent être confirmées par une majorité de nœuds pour être considérées comme réussies.
 mongoose.connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
